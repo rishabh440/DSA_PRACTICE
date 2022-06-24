332f600220bf16abe6bc3b8d42009ad8ef45aa2c
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-06-24 16:55:39 +0530

    Time: 4 ms (83.94%), Space: 45.3 MB (37.37%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,23 +1,25 @@
 class Solution {
-    void findcombinations(int ind , int[] arr , int target ,List<List<Integer>> ans, List<Integer> ds){
-        if(ind==arr.length){
-            if(target==0){
-                ans.add(new ArrayList(ds));
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        func(0 , candidates,target, ans , ds); 
+            return ans ;
+        
+    }
+ void   func(int indx ,int[] candidates, int target , List<List<Integer>> ans, List<Integer> ds ){
+        if(indx == candidates.length){
+            if(target == 0){
+               ans.add(new ArrayList<>(ds));
             }
             return;
         }
         
-        if(arr[ind]<= target){
-            ds.add(arr[ind]);
-            findcombinations(ind , arr , target - arr[ind], ans , ds);
-            ds.remove(ds.size() - 1 );
+        if(candidates[indx]<=target){
+            ds.add(candidates[indx]);
+            func(indx ,candidates , target - candidates[indx], ans , ds );
+           
+            ds.remove(ds.size()-1);
         }
-         findcombinations(ind + 1, arr , target , ans , ds);
-        
-    }
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        findcombinations(0 , candidates , target , ans , new ArrayList<>());
-        return ans;
+      func(indx + 1, candidates, target, ans , ds );
     }
 }

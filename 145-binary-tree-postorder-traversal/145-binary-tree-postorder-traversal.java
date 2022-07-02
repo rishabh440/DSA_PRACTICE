12cf1eed66a2ae0b960927e8b450d1e90c6710be
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-07-03 02:02:44 +0530

    Time: 1 ms (50.41%), Space: 42.5 MB (30.91%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -15,15 +15,32 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-       ArrayList<Integer> ans = new ArrayList<>();
-        ioder(ans , root);
+        Stack <TreeNode> st = new Stack<TreeNode>();
+        ArrayList<Integer> ans = new ArrayList<>();
+          TreeNode node = root;
+     
+      
+        while(node!=null || !st.isEmpty()){
+            if(node!=null){
+                st.push(node);
+                node=node.left;
+            }
+            else{
+             TreeNode  temp = st.peek().right;
+                if(temp==null){
+                    temp = st.peek();
+                    st.pop();
+                    ans.add(temp.val);
+                    while(!st.isEmpty() && temp==st.peek().right){
+                        temp = st.peek();
+                        st.pop();
+                        ans.add(temp.val);
+                    }
+                }else node = temp;
+          
+            }
+           
+        }
         return ans;
-    }
-    void ioder(ArrayList<Integer> ans ,TreeNode node){
-        if(node==null) return ;
-       
-        ioder(ans , node.left);
-        ioder(ans ,node.right);
-        ans.add(node.val);
     }
 }

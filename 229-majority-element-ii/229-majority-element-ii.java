677f980dd416ce972f4b1f4a5313629d427ef244
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-07-06 19:44:21 +0530

    Time: 3 ms (80.93%), Space: 49.8 MB (47.04%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,35 +1,37 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int number1 = -1, number2 = -1, count1 = 0, count2 = 0, len = nums.length;
-    for (int i = 0; i < len; i++) {
-      if (nums[i] == number1)
-        count1++;
-      else if (nums[i] == number2)
-        count2++;
-      else if (count1 == 0) {
-        number1 = nums[i];
-        count1 = 1;
-      } else if (count2 == 0) {
-        number2 = nums[i];
-        count2 = 1;
-      } else {
-        count1--;
-        count2--;
-      }
+        int c1=0 , c2 =0, candi1=0, candi2=0;
+        int fc=0 , fc1 =0;
+        List<Integer> ans = new ArrayList<>();
+        for(int num : nums){
+             if(candi1==num)c1++;
+            else if(candi2==num)c2++;
+             else   if(c1==0){
+                candi1= num;
+                 c1=1;
+            
+            }
+            else if(c2==0){
+                candi2=num;
+                c2=1;
+            }
+            else{
+                c1--;
+                c2--;
+            }
+        }
+        
+        for(int i =0; i<nums.length; i++){
+            if(nums[i]==candi1){
+                fc++;
+            }
+            else if(nums[i]==candi2){
+                fc1++;
+            }
     }
-    ArrayList < Integer > ans = new ArrayList < Integer > ();
-    count1 = 0;
-    count2 = 0;
-    for (int i = 0; i < len; i++) {
-      if (nums[i] == number1)
-        count1++;
-      else if (nums[i] == number2)
-        count2++;
-    }
-    if (count1 > len / 3)
-      ans.add(number1);
-    if (count2 > len / 3)
-      ans.add(number2);
-    return ans;
-  }
-    }
+        
+        if(fc>(nums.length/3)) ans.add(candi1);
+        if(fc1>(nums.length/3)) ans.add(candi2);
+        return ans;
+}
+}

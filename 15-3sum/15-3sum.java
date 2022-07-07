355500f2eed90a998d1bb121c0dc1d5ddd965dc4
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-07-08 02:25:41 +0530

    Time: 32 ms (61.72%), Space: 59.5 MB (39.12%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,37 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-         List<List<Integer>> res = new LinkedList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+    Arrays.sort(nums);
         
-        Arrays.sort(nums);
-        
-        for(int i = 0 ; i< nums.length-2 ; i++){
-            
-            if(i == 0 || (i > 0 && nums[i]!= nums [i-1])){
+        for(int i=0; i<nums.length-2; i++){
+            if(i==0 || (i>0 && nums[i] != nums[i-1])){
+            int ftar= 0-nums[i];
+            int low =i+1;
+            int high= nums.length-1;
+            while(low<high){
+                if(ftar== (nums[low] + nums[high])){
+                    ArrayList<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[low]);
+                    temp.add(nums[high]);
+                    ans.add(temp);
+                    
+                    while(low<high && nums[low]==nums[low+1])low++;
+                    while(low<high && nums[high]==nums[high-1])high--;
+                    low++;
+                    high--;
+                }           
+               else if(ftar > nums[low]+ nums[high]){
+                    low++;
+                }
+                else{
+                    high--;
+                }
                 
-                int lo=i+1, hi = nums.length-1 , sum= 0-nums [i];
-                
-                while(lo<hi){
-                    if((nums[lo]+ nums[hi]) == sum){
-                        res.add(Arrays.asList(nums[i],nums[lo],nums[hi]));
-                        
-                        while(lo<hi && nums[lo]==nums[lo+1])lo++;
-                        while(lo<hi && nums[hi]==nums[hi-1])hi--;
-                        lo++;
-                        hi--;
-                        
-                    }
-                    else if((nums[lo]+ nums[hi])<sum){
-                        lo++;
-                    }
-                       else{
-                           hi--;
-                       }
-            
-                 }
-        
-             }
+            }
+        }   
         }
-        return res;
-   }
- }
-                   
+        return ans;
+        }
+}

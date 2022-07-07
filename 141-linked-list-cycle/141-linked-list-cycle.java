6425e982384f34bdfaca9c2962d1f964ecef6c4a
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-07-07 16:43:54 +0530

    Time: 0 ms (100.00%), Space: 46.5 MB (22.63%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -9,35 +9,15 @@
  *     }
  * }
  */
-// public class Solution {
-//     public boolean hasCycle(ListNode head) {
-//         if(head == null) return false;
-//     ListNode fast = head;
-//     ListNode slow = head;
-        
-//     while(fast.next != null && fast.next.next != null) {
-//         fast = fast.next.next;
-//         slow = slow.next;
-//         if(fast == slow) return true;
-//     }
-//     return false;
-// }
-// }
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode fast = head;
-        ListNode start = new ListNode();
-        start.next = head;
-        ListNode slow = start;
-        if(fast==null){
-            return false;
-        }
-        while(fast!= null && fast.next!=null){
-            
-                fast=fast.next.next;
-                slow= slow.next;
-            if(fast == slow) return true;
-            
+       ListNode slow = head;
+        if(fast==null) return false;
+        while(fast.next!=null && fast.next.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow==fast)return true;
         }
         return false;
     }

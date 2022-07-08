60b118db12844420b47ccf3d7c25dc3dcfbbd097
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-07-09 02:15:44 +0530

    Time: 22 ms (37.25%), Space: 192.4 MB (55.85%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,34 +1,31 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>();
-        List<String> path = new ArrayList<>();
-        func(0, s , res , path);
-        return res;
+        List<List<String>> ans = new ArrayList<>();
+        func(0 , s , ans , new ArrayList<>());
+        return ans;
     }
-    
-    void func(int indx , String s , List<List<String>> res , List<String> path ){
-        if(indx == s.length()){
-            res.add( new ArrayList<>( path));
-            return ;
+   void func(int indx , String s , List<List<String>> ans , List<String> ds){
+        if(indx==s.length()){
+            ans.add(new ArrayList<>(ds));
+            return;
         }
-        
-         for( int i = indx ; i<s.length(); i++){
-             if(palindrome(s , indx , i)){
-                 path.add(s.substring(indx , i + 1));
-                 func(i + 1 , s , res , path);
-                 path.remove( path.size() - 1);
-             }
-             
-         }
-        
+        for(int i= indx ; i<s.length(); i++){
+            if(ispalindrome(s , indx , i)){
+            ds.add(s.substring(indx , i+1));
+            func(i+1 , s , ans , ds);
+            ds.remove(ds.size()-1);
+            }
+        }
     }
-     boolean  palindrome( String s , int start , int end){
-         while(start<= end){
-             if(s.charAt(start++)!= s.charAt(end--)){
-                 return false;
-             }
-             
-         }
-         return  true;
-     }
-}
+ boolean  ispalindrome(String s , int start , int end ){
+        while(start<=end){
+            if(s.charAt(start) == s.charAt(end)){
+                start++;
+                end--;
+            }else {
+                return false;
+            }
+        }
+     return true;
+    }
+    }

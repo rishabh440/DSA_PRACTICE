8508981cc53f3112ac0d2603c38d93a680fa7029
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-07-17 23:02:48 +0530

    Time: 88 ms (55.60%), Space: 135.2 MB (22.30%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,57 +1,59 @@
 class LRUCache {
-  Node head = new Node(0,0) , tail = new Node(0,0);
-    Map<Integer , Node> map = new HashMap<>();
+   Map<Integer , Node > map = new HashMap();
+    Node head = new Node(0,0) , tail = new Node(0,0);
     int cap;
     public LRUCache(int capacity) {
-       cap = capacity;
+        cap = capacity;
         head.next = tail;
-        tail.prev= head;
+        tail.perv = head;
     }
     
     public int get(int key) {
         if(map.containsKey(key)){
-            Node node = map.get(key);
-            remove(node);
+           Node node =  map.get(key);
+      remove(node);
             insert(node);
-            
-           return node.value;
+            return node.val;
         }
         else{
             return -1;
         }
+       
     }
     
     public void put(int key, int value) {
         if(map.containsKey(key)){
-        remove(map.get(key));
+            remove(map.get(key));
         }
-        if(map.size()==cap){
-            remove(tail.prev);
-        }
-        insert(new Node( key ,value));
-    }
-  private void   remove(Node node){
-      map.remove(node.key);
-      node.next.prev = node.prev;
-      node.prev.next= node.next;
-  }
-    private void insert(Node node){
-        map.put(node.key , node);
-        Node headnext = head.next;
-        head.next = node ;
-        headnext.prev = node;
-        node.next= headnext;
-        node.prev = head;
-    }
-    class Node{
-        Node  prev ,next;
-        int key , value;
-            Node(int _key , int _value){
-            key=_key;
-            value=_value;
-        }
+      if(map.size()==cap){
+          remove(tail.perv);
+      }
+        insert(new Node(key , value));
+  
     }
     
+    class Node{
+        Node  perv,next;
+        int key, val;
+        Node( int _key , int _val){
+            key = _key;
+            val = _val;
+        }
+    }
+    private void remove(Node node){
+            map.remove(node.key);
+            node.next.perv = node.perv;
+            node.perv.next = node.next;
+        }
+      private  void insert( Node node){
+            map.put(node.key , node);
+            Node headnext = head.next;
+            head.next = node;
+            headnext.perv = node;
+            node.perv = head;
+            node.next = headnext;
+        }
+            
     
 }
 

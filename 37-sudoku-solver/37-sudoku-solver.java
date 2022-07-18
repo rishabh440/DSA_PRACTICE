7a2c8a76976e75303bd6909cac0f4cfb56049ee4
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-07-18 13:45:05 +0530

    Time: 22 ms (39.23%), Space: 41.7 MB (45.36%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -2,45 +2,40 @@ class Solution {
     public void solveSudoku(char[][] board) {
         func(board);
     }
-  boolean  func( char[][] board){
-        int n = board.length ;
-     int m = board[0].length;
-        for(int i = 0 ; i <9 ; i++ ){
-            for(int j = 0 ; j <9 ; j++){
+    boolean func(char[][] board){
+        for(int i =0 ; i<board.length ; i++){
+            for(int j =0 ; j <board[0].length; j++){
                 if(board[i][j]=='.'){
-                    for(char c = '1'; c<='9'; c++){
-                        if(check(board , i , j , c)){
-                            board[i][j]=c;
-                        
-                        if(func(board)== true){
-                            return true ;
-                        }
-                        else
-                            board[i][j] = '.';
-                        
-                        }
-                    }
+                 for(char c = '1'; c<='9'; c++){
+                     if(isvalid(board, i , j, c)){
+                         board[i][j]=c;
+                         
+                         if(func(board)==true){
+                             return true;
+                         }
+                         else{
+                             board[i][j]= '.';
+                         }
+                     }
+                 }
                     return false;
                 }
-                
             }
         }
         return true;
     }
-    
-    
-    boolean check(char[][]board ,int row , int col , char c){
-        for(int i = 0 ; i < 9; i++){
-            if(board[row][i]== c){
+    boolean isvalid(char[][] board , int row , int col , char c){
+        for(int i =0; i<9 ; i++){
+            if(board[i][col]==c){
                 return false;
             }
-           if(board[i][col]== c){
+            if(board[row][i]==c){
                 return false;
             }
-             if (board[3*(row / 3) + (i / 3)][ 3* (col / 3) + i % 3] == c ){
-                 return false ;
-             }
-           
+            if(board[3*(row/3) + i/3][3*(col/3) + i%3]==c){
+                return false;
+            }
+            
         }
         return true;
     }

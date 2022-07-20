98cca2e50273e20d658d7ef8bced64cec75d91ff
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-07-21 02:03:23 +0530

    Time: 21 ms (5.71%), Space: 42.8 MB (20.68%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -3,8 +3,9 @@ class Solution {
         if(strs.length==0) return "";
         String prefix = strs[0];
         for(int i = 1; i< strs.length;i++){
-             
+               System.out.println("bs" + strs[i].indexOf(prefix));
             while (strs[i].indexOf(prefix) != 0) {
+                System.out.println(strs[i].indexOf(prefix));
              prefix=   prefix.substring(0 , prefix.length() -1);
             }
         }

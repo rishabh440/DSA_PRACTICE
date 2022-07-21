d113e730ecfa948f24c79d78ea5a36c31e695e05
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-07-22 01:40:30 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (55.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,13 +1,23 @@
 class Solution {
     
    public int fib(int n)
-    {
-       int[] dp = new int[n+1];
-        if(n ==0 || n == 1)
+    {   // base cases 
+        if(n == 0 || n == 1)
             return n;
-        else if(dp[n] != 0)
-            return dp[n];
-		else 
-            return dp[n] = fib(n - 1) + fib(n - 2);
+        
+        // take an auxilary space i.e, an array of size one more than the input size
+        int dp[] = new int[n+1];
+        
+        // initialize the base case values in the dp array
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        // now start filling the array so that when the previously computed values are required
+        // we can simply utilize them in order to compute the current value
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];  
+               }
+        
+        return dp[n];
     }
 }

b34c6034932deb4b2cb688b31a765581fb61e18d
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-07-25 16:49:34 +0530

    Time: 0 ms (100.00%), Space: 47.4 MB (31.00%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,24 +2,58 @@ class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] ans = new int[2];
         Arrays.fill(ans, -1);
-        if(nums.length==1 && nums[0]==target){
-            ans[0]=0;
-            ans[1]=0;
-            return ans;
-        }
-        for(int i=0; i<nums.length;i++){
-            if(nums[i]==target){
-                ans[0]=i;
-                break;
+        int lo=0;
+        int high=nums.length-1;
+      
+        while(lo<=high){
+             int  mid=(lo+high)/2;
+            if(nums[mid]==target){
+                if(nums[lo]==target && nums[high]==target){
+                    ans[0]=lo;
+                    ans[1]=high;
+                    break;
+                }
+                if(nums[lo]<target){
+                    lo++;
+                }
+                if(nums[high]>target){
+                    high--;
+                }
             }
-        }
-        if(ans[0]>=0){
-        for(int i=ans[0]; i<nums.length; i++){
-            if(nums[i]== target){
-                ans[1]=i;
+            
+         else if(nums[mid]<target){
+              lo=mid+1;
+          }  
+            else{
+                high=mid-1;
             }
-        }
+            
         }
         return ans;
     }
 }
+// class Solution {
+//     public int[] searchRange(int[] nums, int target) {
+//         int l = 0;
+//         int r = nums.length-1;
+//         while(l <= r){
+//             int mid = (l+r)/2;            
+//             if(nums[mid] == target){
+//                 if(nums[l] == target && nums[r] == target){
+//                     return new int[]{l,r};
+//                 }
+//                 if(nums[l] < target){
+//                     l++;
+//                 }
+//                 if(nums[r] > target){
+//                     r--;
+//                 }
+//             } else if(target < nums[mid]){
+//                 r = mid-1;
+//             } else {
+//                 l = mid+1;
+//             }
+//         }
+//         return new int[]{-1,-1};   
+//     }
+// }

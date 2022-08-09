99b3d95434d7b91f2f1ac246e0e21ba1a55f0db3
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-08-09 14:21:37 +0530

    Time: 11 ms (96.40%), Space: 82.8 MB (8.46%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,49 +10,25 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-    if (head == null || head.next == null)
-      return head;
-        
-    // step 1. cut the list to two halves
-    ListNode prev = null, slow = head, fast = head;
-    
-    while (fast != null && fast.next != null) {
-      prev = slow;
-      slow = slow.next;
-      fast = fast.next.next;
+        ListNode node = head;
+        int cur=0;
+        while(node!=null){
+            node= node.next;
+            cur++;
+        }
+//         cur is the length of linkedlist
+        ListNode and= head;
+        int[] ans = new int[cur]; // puting all values in ans;
+        for(int i=0; i<cur; i++){
+            ans[i]= and.val;
+            and= and.next;
+        }
+        Arrays.sort(ans);  // simpily sorting it
+        ListNode node1= head; // updating the linked list
+        for(int i=0; i<cur; i++){
+            node1.val= ans[i];
+            node1= node1.next;
+        }
+        return head;
     }
-    
-    prev.next = null;
-    
-    // step 2. sort each half
-    ListNode l1 = sortList(head);
-    ListNode l2 = sortList(slow);
-    
-    // step 3. merge l1 and l2
-    return merge(l1, l2);
-  }
-  
-  ListNode merge(ListNode l1, ListNode l2) {
-    ListNode l = new ListNode(0), p = l;
-    
-    while (l1 != null && l2 != null) {
-      if (l1.val < l2.val) {
-        p.next = l1;
-        l1 = l1.next;
-      } else {
-        p.next = l2;
-        l2 = l2.next;
-      }
-      p = p.next;
-    }
-    
-    if (l1 != null)
-      p.next = l1;
-    
-    if (l2 != null)
-      p.next = l2;
-    
-    return l.next;
-  }
-
 }

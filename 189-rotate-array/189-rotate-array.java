c522cafc61d240314de5bc90310fadbda27ee8de
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-08-10 21:18:59 +0530

    Time: 0 ms (100.00%), Space: 64.7 MB (29.61%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,37 +1,38 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
-     if(nums.length <= 1){
-            return;
-        }
-        //step each time to move
-        int step = k % nums.length;
-        int[] tmp = new int[step];
-        for(int i = 0; i < step; i++){
-            tmp[i] = nums[nums.length - step + i];
-        }
-        for(int i = nums.length - step - 1; i >= 0; i--){
-            nums[i + step] = nums[i];
-        }
-        for(int i = 0; i < step; i++){
-            nums[i] = tmp[i];
-        }
+//     public void rotate(int[] nums, int k) {
+//      if(nums.length <= 1){
+//             return;
+//         }
+//         //step each time to move
+//         int step = k % nums.length;
+//         int[] tmp = new int[step];
+//         for(int i = 0; i < step; i++){
+//             tmp[i] = nums[nums.length - step + i];
+//         }
+//         for(int i = nums.length - step - 1; i >= 0; i--){
+//             nums[i + step] = nums[i];
+//         }
+//         for(int i = 0; i < step; i++){
+//             nums[i] = tmp[i];
+//         }
         
-    }
-    
-}
-// public void rotate(int[] nums, int k) {
-//     k %= nums.length;
-//     reverse(nums, 0, nums.length - 1);
-//     reverse(nums, 0, k - 1);
-//     reverse(nums, k, nums.length - 1);
-// }
-
-// public void reverse(int[] nums, int start, int end) {
-//     while (start < end) {
-//         int temp = nums[start];
-//         nums[start] = nums[end];
-//         nums[end] = temp;
-//         start++;
-//         end--;
 //     }
+    
 // }
+public void rotate(int[] nums, int k) {
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+}
+
+public void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
+}

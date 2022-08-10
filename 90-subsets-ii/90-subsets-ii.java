7252becfc30d9ae2877252e1cfd289c1da2e798c
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-08-10 20:37:05 +0530

    Time: 3 ms (49.37%), Space: 44.5 MB (25.86%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,17 +1,13 @@
-class Solution {
+
+ class Solution {
     
     void findSubsets(int ind, int[] nums, List<Integer> ds, List<List<Integer>> ansList) {
         ansList.add(new ArrayList<>(ds)); 
-        
         for(int i = ind;i<nums.length;i++) {
             if(i!=ind && nums[i] == nums[i-1]) continue; 
             ds.add(nums[i]); 
-            
             findSubsets(i+1, nums, ds, ansList); 
-           
             ds.remove(ds.size() - 1);
-             System.out.print(ds.size() + " " );
-           
         }
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {

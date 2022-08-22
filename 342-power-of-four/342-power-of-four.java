cecfc68e70adc2461d7f249b0768d521cc26369c
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-08-22 21:41:40 +0530

    Time: 1 ms (100.00%), Space: 40.9 MB (77.66%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,12 +1,7 @@
 class Solution {
-    public boolean isPowerOfFour(int n) {
-        if(n==1)return true;
-        float nn=n;
-       while(nn>1){
-           
-           nn= nn/4;
-       }
-        if(nn==1)return true;
-        return false;
+    public boolean isPowerOfFour(int num) {
+        return num > 0 && (num&(num-1)) == 0 && (num & 0x55555555) != 0;
+        //0x55555555 is to get rid of those power of 2 but not power of 4
+        //so that the single 1 bit always appears at the odd position 
     }
 }

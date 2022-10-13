6b7883463dc826d5944b7b0a128519fd6a0d7a27
rishabh440 <75711507+rishabh440@users.noreply.github.com>
2022-10-13 18:12:23 +0530

    Time: 1 ms (97.46%), Space: 54.3 MB (27.46%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -12,12 +12,11 @@ class Solution {
         }
         
         for(int i=m.length-1; i>=0;i--){
-            // if(col==0)m[i][0]=0;
+         
             for(int j=m[0].length-1; j>=1; j--){
                 if(m[i][0]==0 || m[0][j]==0){
                     m[i][j]=0;
                 }
-                 // if(col==0)m[i][0]=0;
             }
              if(col==0)m[i][0]=0;
         }
